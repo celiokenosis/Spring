a42a6996f7c19e130c9969c5d672b29c0dc7a3e8
CÉLIO MENDES PEREIRA <78619443+celiokenosis@users.noreply.github.com>
2022-10-04 09:55:05 -0300

    Add files via upload

---
+++ acesso.postgresql/acesso.postgresql/src/main/java/acesso/postgresql/com/example/acesso/postgresql/Application.java
@@ -0,0 +1,13 @@
+package acesso.postgresql.com.example.acesso.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ algalog-api/src/main/java/com/algaworks/algalog/api/exceptionHandler/ApiExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.algaworks.algalog.api.exceptionHandler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+
+}
