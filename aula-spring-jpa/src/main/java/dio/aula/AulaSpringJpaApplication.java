9376414ef93d39ae35eaecb0ca92d6cfb0d593d8
CÉLIO MENDES PEREIRA <78619443+celiokenosis@users.noreply.github.com>
2022-10-07 15:47:26 -0300

    Add files via upload

---
+++ aula-spring-jpa/src/main/java/dio/aula/AulaSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package dio.aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpringJpaApplication.class, args);		
+	}	
+
+}
