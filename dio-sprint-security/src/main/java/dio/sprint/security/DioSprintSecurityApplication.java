2a82b36fc37adf08aae93b586a4fd19e04730c91
CÉLIO MENDES PEREIRA <78619443+celiokenosis@users.noreply.github.com>
2022-10-10 16:00:36 -0300

    Add files via upload

---
+++ dio-sprint-security/src/main/java/dio/sprint/security/DioSprintSecurityApplication.java
@@ -0,0 +1,13 @@
+package dio.sprint.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSprintSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSprintSecurityApplication.class, args);
+	}
+
+}
---
+++ dio-sprint-security/src/test/java/dio/sprint/security/DioSprintSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.sprint.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSprintSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
